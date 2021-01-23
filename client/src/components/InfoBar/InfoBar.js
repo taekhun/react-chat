603c50b1115d4ab3180f5e8d3ff40de1be5e2afa
taekhun <nth9708@naver.com>
2021-01-23 15:32:49 +0900

    :boom: CORS issue

---
+++ client/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,20 @@
+import "./InfoBar.css";
+
+import closeIcon from "../../icons/closeIcon.png";
+import onlineIcon from "../../icons/onlineIcon.png";
+
+const InfoBar = ({ room }) => (
+  <div className='infoBar'>
+    <div className='leftInnerContainer'>
+      <img className='onlineIcon' src={onlineIcon} alt='online' />
+      <h3>{room}</h3>
+    </div>
+    <div className='rightInnerContainer'>
+      <a href='/'>
+        <img claasName='closeIcon' src={closeIcon} alt='close' />
+      </a>
+    </div>
+  </div>
+);
+
+export default InfoBar;
